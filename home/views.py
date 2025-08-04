979d38248582257638f0b29cf057126105043877
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-04 14:02:32 +0200

    Add meta tags, favicon, and Bootstrap setup

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("CozyNest!")
+    """ A view to return the home page """
+
+    return render(request, 'home/home.html')
