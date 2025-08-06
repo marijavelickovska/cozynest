1d6a3ec1c4fffc3f5b318358c5e0bfee1f94b396
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-06 12:51:48 +0200

    Add models for products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+class Size(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    hex_code = models.CharField(max_length=7, blank=True, null=True) 
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True)
+    base_price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.name
+
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='variants')
+    size = models.ForeignKey(Size, on_delete=models.PROTECT)
+    color = models.ForeignKey(Color, on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    sku = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='variants/', blank=True, null=True)  # optional: if variant has specific image
+
+    class Meta:
+        ordering = ["-product__created_on"]
+        unique_together = ('product', 'size', 'color')  # ensures only one variant with same size & color
+
+    def __str__(self):
+        return f"{self.product.name} - {self.size.name} - {self.color.name} - (SKU: {self.sku})"
