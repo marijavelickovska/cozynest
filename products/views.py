e7089bd139bbcc019254c10f55d287757ffc2304
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-10 14:04:56 +0200

    Add sorting and filtering functionality to products list

--- products/views.py
+++ products/views.py
@@ -1,37 +1,62 @@
-from django.shortcuts import render, redirect
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 
 def all_products(request):
-    """A view to show all products, including sorting and search queries"""
-
     products = Product.objects.all()
+    query = None
+    category = None
+    category_obj = None
+    current_sorting = request.GET.get('sort', 'None_None')
 
-    category = request.GET.get('category', None)
+    if request.GET:
+        if 'category' in request.GET:
+            category = request.GET.get('category')
+            products = products.filter(category__name__iexact=category)
 
-    if category:
-        products = products.filter(category__name__iexact=category)
+            category_obj = get_object_or_404(Category, name__iexact=category)
 
-    sort = request.GET.get("sort", None)
-    direction = request.GET.get("direction", None)
+        if 'sort' in request.GET:
+            sort_full = request.GET.get('sort')
+            if '_' in sort_full:
+                sort, direction = sort_full.rsplit('_', 1)
+            else:
+                sort = sort_full
+                direction = 'asc'
 
-    if sort:
-        sortkey = sort
+            if sort == 'category':
+                sortkey = 'category__name'
+            elif sort == 'rating':
+                sortkey = 'rating'
+            elif sort == 'base_price':
+                sortkey = 'base_price'
+            elif sort == 'name':
+                sortkey = 'name'
+            else:
+                sortkey = sort
 
-        if sort == 'category':
-            sortkey = 'category__name'
-        elif sort == 'rating':
-            sortkey = 'rating'
-        elif sort == 'base_price':
-            sortkey = 'base_price'
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
 
-        if direction == "desc":
-            sortkey = f"-{sortkey}"
+            products = products.order_by(sortkey)
 
-        products = products.order_by(sortkey)
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
-        "products": products,
+        'products': products,
+        'search_term': query,
+        'current_category': category,
+        'category_obj': category_obj,
+        'current_sorting': current_sorting
     }
 
     return render(request, "products/products.html", context)
