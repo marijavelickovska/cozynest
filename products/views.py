c9b50fde52eb77900e5af02af3c8e860ff45d6ec
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-11 11:19:00 +0200

    Add functionality to display sizes and colors on product detail page

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category
+from .models import Product, Category, Size, Color
 
 
 def all_products(request):
@@ -66,9 +66,13 @@ def product_detail(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
+    sizes = product.category.sizes.all() if product.category else Size.objects.none()
+    colors = Color.objects.filter(productvariant__product=product).distinct()
 
     context = {
         'product': product,
+        'sizes': sizes,
+        'colors': colors,
     }
 
     return render(request, 'products/product_detail.html', context)
