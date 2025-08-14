c39955846e398fa41551ea5e0767beddff195771
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-14 14:53:38 +0200

    Add view to handle UserUpdateForm

--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
-from .forms import UserProfileForm
+from .forms import UserProfileForm, UserUpdateForm
 
 
 @login_required
@@ -12,16 +12,28 @@ def profile(request):
     tab = request.resolver_match.url_name
 
     form = None
+
     if tab == "delivery_information":
         if request.method == "POST":
             form = UserProfileForm(request.POST, instance=profile)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Delivery informations updated successfully")
+            else:
+                messages.error(request, "Update failed. Please ensure the form is valid.")
+        else:  # GET request
+            form = UserProfileForm(instance=profile)
+
+    if tab == "profile":
+        if request.method == "POST":
+            form = UserUpdateForm(request.POST, instance=profile)
             if form.is_valid():
                 form.save()
                 messages.success(request, "Profile updated successfully")
             else:
                 messages.error(request, "Update failed. Please ensure the form is valid.")
         else:  # GET request
-            form = UserProfileForm(instance=profile)
+            form = UserUpdateForm(instance=profile)
 
     context = {"form": form, "tab": tab, "on_profile_page": True}
     return render(request, "profiles/profile_base.html", context)
