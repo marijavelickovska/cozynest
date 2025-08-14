725059cd6c9c07dccfdb262791ef08ff7f9c1ca4
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-14 14:53:01 +0200

    Add UserUpdateForm and display on profile template

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 from .models import UserProfile
+from django.contrib.auth.models import User
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
 
 
 class UserProfileForm(forms.ModelForm):
