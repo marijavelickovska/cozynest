4bf9f7ed1b0260776c4edade00804eb782d786ea
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-14 12:24:40 +0200

    Update user profile layout and add styles

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('delivery_information', views.profile, name='delivery_information'),
+    path('order_history', views.profile, name='order_history'),
 ]
