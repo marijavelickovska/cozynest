b7a682fe5f6f8aac43c044750c0ad64ee215963c
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-16 13:03:23 +0200

    Add Stripe setup in settings and URLs

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path("create_payment_intent/", views.create_payment_intent, name="create_payment_intent"),
     path('', views.checkout, name='checkout'),
+    path('success/', views.checkout_success, name='checkout_success'),
 ]
