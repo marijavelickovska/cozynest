fb8b4b51430a8cdcc99c6aa6a303904ea64824b0
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-19 12:33:13 +0200

    Update profile view to show order history and add order_detail view with URLs

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm, UserUpdateForm
+from checkout.models import Order
 
 
 @login_required
@@ -11,19 +12,8 @@ def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     user = request.user
     tab = request.resolver_match.url_name
-
     form = None
-
-    if tab == "delivery_information":
-        if request.method == "POST":
-            form = UserProfileForm(request.POST, instance=profile)
-            if form.is_valid():
-                form.save()
-                messages.success(request, "Delivery informations updated successfully")
-            else:
-                messages.error(request, "Update failed. Please ensure the form is valid.")
-        else:  # GET request
-            form = UserProfileForm(instance=profile)
+    orders = None
 
     if tab == "profile":
         if request.method == "POST":
@@ -36,5 +26,35 @@ def profile(request):
         else:  # GET request
             form = UserUpdateForm(instance=user)
 
-    context = {"form": form, "tab": tab, "on_profile_page": True}
+    if tab == "delivery_information":
+        if request.method == "POST":
+            form = UserProfileForm(request.POST, instance=profile)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Delivery informations updated successfully")
+            else:
+                messages.error(request, "Update failed. Please ensure the form is valid.")
+        else:  # GET request
+            form = UserProfileForm(instance=profile)
+
+    if tab == "order_history":
+        orders = profile.orders.all()
+
+    context = {
+        "form": form,
+        "tab": tab,
+        "orders": orders,
+        "on_profile_page": True}
+
     return render(request, "profiles/profile_base.html", context)
+
+
+def order_detail(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number, user_profile=request.user.userprofile)
+
+    context = {
+        "order": order,
+        "tab": "order_history",
+    }
+
+    return render(request, "profiles/order_detail.html", context)
