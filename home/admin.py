7b72ba241baecf643719bb21b633da8871fe7312
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-21 13:34:17 +0200

    Add ContactMessage model and admin configuration

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the ContactMessage model.
+    Displays sender name, email and message,
+    allows search and filtering by name and email.
+    """
+    list_display = ('name', 'email', 'message', 'created_on')
+    search_fields = ('name', 'email', 'message')
+    list_filter = ('created_on',)
