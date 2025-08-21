4c9833d67174fe6b00c2dfbfff9c7c739f02a330
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-21 13:35:11 +0200

    Add URL, view, and template for Contact Us page

--- home/views.py
+++ home/views.py
@@ -11,3 +11,9 @@ def about_us(request):
     """ A view to return the about us page """
 
     return render(request, 'home/about_us.html')
+
+
+def contact_us(request):
+    """ A view to return the contact us page """
+
+    return render(request, 'home/contact_us.html')
