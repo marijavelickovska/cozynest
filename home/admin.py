dfa10b956c9de20739f0d40546eebb110b8d72a1
marijavelickovska <marija_velickovska88@hotmail.com>
2025-08-24 14:59:17 +0200

    Register Newsletter model in admin

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ContactMessage
+from .models import ContactMessage, Newsletter
 
 
 @admin.register(ContactMessage)
@@ -12,3 +12,14 @@ class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'message', 'created_on')
     search_fields = ('name', 'email', 'message')
     list_filter = ('created_on',)
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Newsletter model.
+    Displays subscriber emails and the date they subscribed.
+    """
+    list_display = ("email", "created_at")
+    search_fields = ("email",)
+    ordering = ("-created_at",)
